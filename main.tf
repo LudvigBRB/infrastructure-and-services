7b82d5d9661f06d343c16b76f32b7370ee2528d8
ITE17 <cph-lb270@cphbusiness.dk>
2021-03-20 15:04:47 +0100

    added kubernetes to ressource file

--- main.tf
+++ main.tf
@@ -1,3 +1,9 @@
 provider "kubernetes" {
   config_context_cluster   = "minikube"
 }
+
+resource "kubernetes_namespace" "1-minikube-namespace" {
+  metadata {
+        name = "my-first-terraform-namespace"
+  }
+}
