b6b51aa32896940b807f6834fd376e0c9c8d0e6e
ITE17 <cph-lb270@cphbusiness.dk>
2021-03-21 12:42:58 +0100

    made jenkins and helm files

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "kubernetes" {
   config_context_cluster   = "minikube"
 }
 
-resource "kubernetes_namespace" "1-minikube-namespace" {
+resource "kubernetes_namespace" "hello-minikube" {
   metadata {
         name = "my-first-terraform-namespace"
   }
